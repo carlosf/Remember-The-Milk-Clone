53a67cd07c144a8bd5d775e2a0f8325cc03cccd7
Vipul A M <vipulnsward@gmail.com>
2011-07-12 23:34:42 +0530

    Commit with all files

---
+++ todo/col.bal/documents.py
@@ -0,0 +1,17 @@
+from collection import db
+from flaskext.mongoalchemy import BaseQuery
+import datetime
+import re
+
+class BookQuery(BaseQuery):
+
+    def starting_with(self, letter):
+        regex = r'^' + letter
+        return self.filter({'title' : re.compile(regex, re.IGNORECASE)})
+
+class Book(db.Document):
+    query_class = BookQuery
+    
+    title = db.StringField()
+    year = db.IntField()
+    time =db.IntField()
