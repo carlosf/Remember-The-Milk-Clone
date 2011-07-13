3a2a771a3f0ee746ca2348dbcaec6ebf64645385
Vipul A M <vipulnsward@gmail.com>
2011-07-13 11:21:57 +0530

    Fixed unused Babel

--- todo/collection/__init__.py
+++ todo/collection/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flaskext.mongoalchemy import MongoAlchemy
-from flaskext.babel import Babel
 import string
 
 app = Flask(__name__)
@@ -8,7 +7,6 @@ app.config['MONGOALCHEMY_DATABASE'] = 'books_collection'
 app.config['SECRET_KEY'] = 'very secret, do you believe?'
 app.config['DEBUG'] = True
 db = MongoAlchemy(app)
-babel=Babel(app)
 
 
 @app.context_processor
