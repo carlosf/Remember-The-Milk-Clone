d1fa9ae65277358f0ca528a4c70b86ce5cbb9de7
Vipul A M <vipulnsward@gmail.com>
2011-07-13 19:40:14 +0530

    Fixed jscript redundant code and task references

--- todo/collection/__init__.py
+++ todo/collection/__init__.py
@@ -3,14 +3,9 @@ from flaskext.mongoalchemy import MongoAlchemy
 import string
 
 app = Flask(__name__)
-app.config['MONGOALCHEMY_DATABASE'] = 'books_collection'
+app.config['MONGOALCHEMY_DATABASE'] = 'task_collection'
 app.config['SECRET_KEY'] = 'very secret, do you believe?'
 app.config['DEBUG'] = True
 db = MongoAlchemy(app)
-
-
-@app.context_processor
-def put_letters_on_request():
-    return { 'letters' : string.ascii_uppercase }
 
 from views import *
